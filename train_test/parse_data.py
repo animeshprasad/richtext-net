c6907929ed1907afcbc0e48d65b69fb3b9b1294c
NUS <nus@animeshs-mbp.lan>
2018-11-29 11:21:31 +0800

    multiple indices per mention

--- train_test/parse_data.py
+++ train_test/parse_data.py
@@ -5,10 +5,12 @@ import pandas as pd
 import os, sys, random
 from nltk.tokenize import  WordPunctTokenizer
 from collections import Counter
-import json, codecs
+import json, codecs, re
 
 reload(sys)
 sys.setdefaultencoding('utf8')
+
+MAX_LENGTH=30
 
 class DataParser:
 
@@ -44,9 +46,9 @@ class DataParser:
 
 
 
-    def get_train_data_full(self, neg_ratio):
-        max_length_token = 100
-        pos_neg_sample_ratio = neg_ratio # out of 100
+    def get_train_data_full(self, neg_ratio=1):
+        max_length_token = MAX_LENGTH
+        pos_neg_sample_ratio = neg_ratio #5 out of 1000
 
         pos_count = 0
         neg_count = 0
@@ -61,11 +63,38 @@ class DataParser:
                 pos_splits = []
                 for mention in row['mention_list']:
                     mention_text = mention.encode('utf-8', 'replace')
+                    mention_text = re.sub('\d', '0', mention_text)
+                    mention_text = re.sub('[^ ]- ', '', mention_text)
                     mention_text_spans = list(WordPunctTokenizer().span_tokenize(mention_text))
 
-                    find_index = sample_text.find(mention_text)
 
-                    try:
+                    def findall(p, s):
+                        '''Yields all the positions of
+                        the pattern p in the string s.'''
+                        i = s.find(p)
+                        while i != -1:
+                            yield i
+                            i = s.find(p, i + 1)
+
+                    def findall_lower(p, s):
+                        i = s.lower().find(p.lower())
+                        while i != -1:
+                            yield i
+                            i = s.lower().find(p.lower(), i + 1)
+
+
+                    index_finder = findall(mention_text, sample_text)
+                    index_finder_lower = findall_lower(mention_text, sample_text)
+
+                    found_indices = [idx for idx in index_finder]
+                    found_indices_lower = [idx for idx in index_finder_lower]
+
+                    all_found_indices = list(set(found_indices + found_indices_lower))
+
+
+
+                    for find_index in all_found_indices:
+                      try:
                         if find_index != -1:
                             mention_text_spans = [(indices[0] + find_index, indices[1] + find_index) for indices in mention_text_spans]
                             #write to training sample pointers here
@@ -75,12 +104,10 @@ class DataParser:
                                   sample_text_spans.index(mention_text_spans[-1]) < (splits+2)*(max_length_token/2):
 
                                     pos_splits.append(splits)
-
                                     pos_count += 1
 
-                                    #TODO Wrapper over full data_b reader
+                                    #TODO Wrapper over full data reader
                                     golden_data.write(
-                                        #str(row['publication_id']) + ' ' + str(row['data_set_id']) + ' ' +
                                         str(sample_text_spans.index(mention_text_spans[0]) - splits*(max_length_token/2)) +
                                         ' ' + str(sample_text_spans.index(mention_text_spans[-1]) - splits*(max_length_token/2)) +
                                         ' ' + str(row['data_set_id']) + ' ' + str(row['publication_id']) +
@@ -88,18 +115,34 @@ class DataParser:
                                         + '\n'
                                     )
                         else:
-                            print ('Annotation Error')
-                    except:
-                        print ('Indexing Logic Error')
+                            print ('Annotation Error: Annotated gold standards not correct')
+                            pass
+                      except:
+                        #print ('Indexing Logic Error: Some corner index case missed while parsing')
+                        pass
 
-                for splits in range(len(sample_text_tokens) / (max_length_token/2) - 2):
-                    if splits not in pos_splits and random.randint(0, 100) < pos_neg_sample_ratio:
+                        # print mention_text_spans
+                        # print (find_index)
+                        # print (sample_text[mention_text_spans[0][0]: mention_text_spans[-1][1]])
+                        # print (mention_text)
+                        #
+                        # for i in findall_lower(mention_text, sample_text):
+                        #     print i
+                        #
+                        # print (find_index, sample_text_spans.index(mention_text_spans[0]),  sample_text_spans.index(mention_text_spans[-1]),
+                        #        splits * (max_length_token), (splits+1)*(max_length_token))
+                        #
+                        # print mention_text, row['publication_id']
+                        # raw_input()
+
+
+                for splits in range(len(sample_text_tokens) / (max_length_token / 2) - 2):
+                    if splits not in pos_splits and random.randint(0, 1000) < pos_neg_sample_ratio:
                         golden_data.write(
-                            #str(row['publication_id']) + ' ' + str(0) + ' ' +
-                            str(0) +
-                            ' ' + str(0) +
-                            ' ' + str(row['data_set_id']) + ' ' + str(row['publication_id']) +
-                            ' ' + ' '.join(sample_text_tokens[splits * (max_length_token / 2):(splits + 2) * (max_length_token / 2)])
+                            str(0) + ' ' + str(0) +
+                            ' ' + str(0) + ' ' + str(row['publication_id']) +
+                            ' ' + ' '.join(sample_text_tokens[splits * (max_length_token / 2):(splits + 2) * (
+                                                max_length_token / 2)])
                             + '\n'
                         )
 
@@ -153,11 +196,71 @@ class DataParser:
                         lines = f.readlines()
                         #TODO document structure
                         text = ' '.join([s.strip() for s in lines]).decode('utf-8', 'replace')
-                        text.replace('- ', '')
+                        text = re.sub('\d', '0', text)
+                        text = re.sub('[^ ]- ', '', text)
                         full_text[item] = text
                     except:
                         pass
         return full_text
+
+
+class DummydataParser:
+
+    def __init__(self):
+        pass
+
+    def _extract(self, dir_name='files/text/', extension='.txt'):
+        current_dir = os.getcwd()
+        dir_name = current_dir + '/' + dir_name
+        full_text = {}
+        for item in os.listdir(dir_name):
+            if item.endswith(extension):
+                file_name = os.path.abspath(dir_name + '/' + item)
+                with codecs.open(file_name, 'rb') as f:
+                    try:
+                        lines = f.readlines()
+                        #TODO document structure
+                        text = ' '.join([s.strip() for s in lines]).decode('utf-8', 'replace')
+                        text = re.sub('\d', '0', text)
+                        text = re.sub('[^ ]- ', '', text)
+                        full_text[item] = text
+                    except:
+                        pass
+        return full_text
+
+    def get_predict_split(self):
+        self.data_set_citations = pd.read_json('data_set_citations.json', encoding='utf-8')
+        self.data_set = pd.read_json('data_sets.json', encoding='utf-8')
+        self.publications = pd.read_json('publications.json', encoding='utf-8')
+
+        self.data_set_citations.drop(self.data_set_citations.index, inplace=True)
+        self.data_set.drop(self.data_set.index, inplace=True)
+        self.publications.drop(self.publications.index, inplace=True)
+
+
+        self.publications.insert(6, 'full_text', pd.Series)
+        self.full_text = self._extract()
+        full_text_series = pd.Series()
+
+        # count = 0
+        # for i, file_id in zip(self.publications.index, self.publications['publication_id']):
+        #     try:
+        #         full_text_series.loc[i] = self.full_text[str(file_id) + '.txt']
+        #         count += 1
+        #     except:
+        #         print(str(file_id) + '.txt not found in files')
+        #         pass
+        #
+        # self.publications['full_text'] = full_text_series
+        #
+        # for splits in range(len(sample_text_tokens) / (max_length_token) - 1):
+        #     if splits not in pos_splits and random.randint(0, 1000) < pos_neg_sample_ratio:
+        #         golden_data.write(
+        #             str(0) + ' ' + str(0) +
+        #             ' ' + str(0) + ' ' + str(row['publication_id']) +
+        #             ' ' + ' '.join(sample_text_tokens[splits * (max_length_token):(splits + 1) * (max_length_token)])
+        #             + '\n'
+        #         )
 
 
 class OuputEvaluator:
@@ -186,5 +289,5 @@ class OuputParser:
 
 if __name__ == '__main__':
     data_parser = DataParser(outdir='../data/')
+    data_parser.get_train_data_full()
     data_parser.get_vocab()
-    data_parser.get_train_data_full(5)
